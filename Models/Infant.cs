f48f8fa2dc9cf8caa47381d9ef3b509a884b7b1c
bobby-dq <josh.quilacio@gmail.com>
2021-02-05 21:23:42 -0700

    Init models.

---
+++ Models/Infant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BabyTracker.Models
+{
+    public class Infant
+    {
+        public long InfantId { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(Length:)]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
+
+    }
+}
