f0b293dea7310d3d0d3b529c54bdaecf95fe8eed
bobby-dq <josh.quilacio@gmail.com>
2021-02-13 19:31:51 -0700

    Migrated the database seed and initialized the database connection string.

---
+++ Migrations/20210214023040_Initial.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BabyTracker.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Infants",
+                columns: table => new
+                {
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dob = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Infants", x => x.InfantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diapers",
+                columns: table => new
+                {
+                    DiaperId = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DiaperType = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diapers", x => x.DiaperId);
+                    table.ForeignKey(
+                        name: "FK_Diapers_Infants_InfantId",
+                        column: x => x.InfantId,
+                        principalTable: "Infants",
+                        principalColumn: "InfantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedings",
+                columns: table => new
+                {
+                    FeedingId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FeedType = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Duration = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedings", x => x.FeedingId);
+                    table.ForeignKey(
+                        name: "FK_Feedings_Infants_InfantId",
+                        column: x => x.InfantId,
+                        principalTable: "Infants",
+                        principalColumn: "InfantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Growths",
+                columns: table => new
+                {
+                    GrowthId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GrowthType = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Growths", x => x.GrowthId);
+                    table.ForeignKey(
+                        name: "FK_Growths_Infants_InfantId",
+                        column: x => x.InfantId,
+                        principalTable: "Infants",
+                        principalColumn: "InfantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medications",
+                columns: table => new
+                {
+                    MedicationId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MedicationType = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medications", x => x.MedicationId);
+                    table.ForeignKey(
+                        name: "FK_Medications_Infants_InfantId",
+                        column: x => x.InfantId,
+                        principalTable: "Infants",
+                        principalColumn: "InfantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sleeps",
+                columns: table => new
+                {
+                    SleepId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Comments = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InfantId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sleeps", x => x.SleepId);
+                    table.ForeignKey(
+                        name: "FK_Sleeps_Infants_InfantId",
+                        column: x => x.InfantId,
+                        principalTable: "Infants",
+                        principalColumn: "InfantId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diapers_InfantId",
+                table: "Diapers",
+                column: "InfantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedings_InfantId",
+                table: "Feedings",
+                column: "InfantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Growths_InfantId",
+                table: "Growths",
+                column: "InfantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medications_InfantId",
+                table: "Medications",
+                column: "InfantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sleeps_InfantId",
+                table: "Sleeps",
+                column: "InfantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Diapers");
+
+            migrationBuilder.DropTable(
+                name: "Feedings");
+
+            migrationBuilder.DropTable(
+                name: "Growths");
+
+            migrationBuilder.DropTable(
+                name: "Medications");
+
+            migrationBuilder.DropTable(
+                name: "Sleeps");
+
+            migrationBuilder.DropTable(
+                name: "Infants");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -24,10 +24,15 @@ namespace BabyTracker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BabyTrackerContext>(opts => 
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:BabyTrackerConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BabyTrackerContext context)
         {
             if (env.IsDevelopment())
             {
@@ -43,6 +48,8 @@ namespace BabyTracker
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            SeedData.SeedDatabase(context);
         }
     }
 }
