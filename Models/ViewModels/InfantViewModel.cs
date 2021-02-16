a3c24304dc308c203d6bdb04b4c5a3a89e525846
bobby-dq <josh.quilacio@gmail.com>
2021-02-16 00:14:37 -0700

    Initialized the jquery library.

--- Models/ViewModels/InfantViewModel.cs
+++ Models/ViewModels/InfantViewModel.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace BabyTracker.Models.ViewModels
 {
     public class InfantViewModel
     {
-        
+        public Infant Infant {get; set;}
+        public string Action {get;set;}
+        public bool ReadOnly {get; set;}
+        public string Theme {get;set;}
+        public bool ShowAction {get; set;}
     }
 }
