dac6e5df0c850990b70ad19a8e7b6c09b9cd53eb
bobby-dq <josh.quilacio@gmail.com>
2021-03-04 17:22:02 -0700

    Tidied up the delete for roles.

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Westwind.AspNetCore.LiveReload;
 using BabyTracker.Models;
 using BabyTracker.Models.ViewModels;
 using BabyTracker.Models.RepositoryModels;
